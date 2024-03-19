4342ca7db15ca5aee9d7d975c1a0d6cf392bd9a4
pnaruka <pankajnaruka111@gmail.com>
2024-03-19 15:00:42 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const {Schema} = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email :{
+        type: String,
+        required: true,
+        unique: true
+    },
+    profileImg: {
+        type: String,
+        default: "/images/userDefault.png"
+    },
+    role:{
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER"
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},
+    {timestamps: true}
+);
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(!user.isModified("password"))
+    return;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    this.password = hashedPassword;
+    next();
+
+})
+
+const User = model('user',userSchema);
+
+module.exports = {
+    User
+}
