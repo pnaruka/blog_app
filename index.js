01568b5dbcf4c25f7e404ea28ba6cb5d43e92c5b
pnaruka <pankajnaruka111@gmail.com>
2024-03-19 14:12:54 +0530

    navbar added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 8000 || process.env.PORT;
 app.set("view engine", "ejs");
 app.set("views",path.resolve("./views"))
 
+app.get("/",(req, res)=>{
+    res.render("home");
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server up on port: ${PORT}`);
 });
